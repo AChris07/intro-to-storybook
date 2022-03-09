559c369f558a3773e7d1bf7680639c411fe0f7d6
Cristiano Amici <cristiano.amici@herodigital.com>
2022-03-08 22:53:24 -0500

    Change Globals to use Emotion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
-import { ThemeProvider } from '@emotion/react';
+import { Global, ThemeProvider } from '@emotion/react';
 
 import { AppPropsWithLayout } from 'src/types/pages';
+import globals from 'src/styles/globals';
 import theme from 'src/styles/theme';
-import 'src/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <ThemeProvider theme={theme}>{getLayout(<Component {...pageProps} />)}</ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <Global styles={globals} />
+      {getLayout(<Component {...pageProps} />)}
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/globals.ts
@@ -0,0 +1,15 @@
+export default () => ({
+  'html, body': {
+    padding: 0,
+    margin: 0,
+    fontFamily: `-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;`,
+  },
+  a: {
+    color: 'inherit',
+    textDecoration: 'none',
+  },
+  '*': {
+    boxSixing: 'border-box',
+  },
+});
